a9f18732714eff64b92ea81ea7ee6df712e319d8
ricardosexenes <edgar_ss2012@hotmail.com>
2025-03-29 16:36:22 -0600

    se termino de crear las tranciones para las paginas y los componentes

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import CoverParticles from "@/components/cover-particles";
+import TransitionPage from "@/components/transition-page";
 import Image from "next/image";
 
 export default function Home() {
   return (
 
     <main>
+      <TransitionPage></TransitionPage>
       <div className="flex min-h-[100vh] h-full bg-no-repeat bg-gradient-cover">
         <CoverParticles></CoverParticles>
         <p>Introduccion</p>
