48bb039d5051025cc44f6603c0b484a4262d164e
ricardosexenes <edgar_ss2012@hotmail.com>
2025-04-06 15:52:55 -0600

    ya se corrigio portafolio

--- app/(routes)/services/page.tsx
+++ app/(routes)/services/page.tsx
@@ -10,12 +10,12 @@ const ServicesPage = () => {
 
             <TransitionPage />
             <CircleImage />
-            <AvatarServices />
-            <div className="grid items-center justify-center h-screen max-w-5xl gap-6 mx-auto md:grid-cols-2">
-                <div className="max-w-[450px]">
+            <AvatarServices /><div className="p-6 grid items-center justify-center h-screen max-w-5xl gap-6 mx-auto md:grid-cols-2 pt-10">
+
+                <div className="max-w-[450px] mt-[7.5rem]">
 
                     <h1 className="text-2xl leading-tight text-center md:text-left md:text-4xl md:mb-5">Mis <span className="font-bold text-secondary"> servicios.</span></h1>
-                    <p className="mb-3 text-xl text-gray-300">Principalmente ofrezco soluciones como sistemas web personalizados con enfoque en backend utilizando PHP y MySQL. Creo soluciones eficientes, seguras y adaptadas a las necesidades de cada cliente, como sistemas POS (punto de venta), PMS, gestión de citas o reservaciones, entre otros. Además, diseño interfaces modernas y responsivas usando HTML, CSS (con SASS), JavaScript y Bootstrap, ofreciendo plataformas completas y funcionales, ademas de soporte de redes y mantenimiento preventivo en equipos de computo.</p>
+                    <p className="mb-3 text-xl text-gray-300">Ofrezco soluciones web personalizadas con enfoque en backend utilizando PHP y MySQL, como sistemas POS, PMS, y gestión de citas. También diseño interfaces modernas y responsivas con HTML, CSS (SASS), JavaScript y Bootstrap, además de proporcionar soporte en redes y mantenimiento preventivo en equipos de cómputo.</p>
                     <button className="px-3 py-2 rounded-lg bg-secondary hover:bg-secondary/65">Contacta conmigo</button>
                 </div>
 
