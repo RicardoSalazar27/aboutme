c0d0bd31fcc2c92f5da8075cb84954cc1dc5fbeb
ricardosexenes <edgar_ss2012@hotmail.com>
2025-04-19 11:43:35 -0600

    solo faltan los testimoniales y imagenes del portafolio

--- components/introduction.tsx
+++ components/introduction.tsx
@@ -6,9 +6,9 @@ import { TypeAnimation } from "react-type-animation";
 const Introduction = () => {
     return( 
         <div className="z-20 w-full bg-darkBg/60">
-            {/* <div className="z-20 grid items-center h-full p-6 py-20 md:py-0 md:grid-cols-2 "></div> */}
-                <div className="z-20 grid items-center h-full p-6 pt-32 md:pt-20 md:py-0 md:grid-cols-2">
-                <Image src="/home-4.png" priority width="800" height="800" alt="Profile pic"></Image>
+            <div className="z-20 grid items-center h-full p-6 py-20 md:py-0 md:grid-cols-2 ">
+            {/* <div className="z-20 grid items-center h-full p-6 pt-32 md:pt-20 md:py-0 md:grid-cols-2"> */}
+                <Image src="/home-4.png" priority width="600" height="600" alt="Profile pic"></Image>
 
                 <div className="flex flex-col jusitfy-center max-w-md">
                     <h1 className="mb-5 text-2xl leading-right text-center md:text-left md:text-4xl md:mb-10">Si puedes imaginarlo, <br></br>
@@ -33,10 +33,10 @@ const Introduction = () => {
                             Como desarrollador full stack e ingeniero en sistemas, me especializo en construir soluciones web completas que combinan rendimiento, escalabilidad y una experiencia de usuario sólida de principio a fin.
                         </p>
                         <div className="flex items-center justify-center gap-3 md:justify-start md:gap-10">
-                        <a href="/projects" className="px-3 py-2 my-2 transition-all border-2 cursor-pointer text-md w-fit rounded-xl hover:shadow-xl hover:shadow-white/50">
+                        <a href="/portfolio" className="px-3 py-2 my-2 transition-all border-2 cursor-pointer text-md w-fit rounded-xl hover:shadow-xl hover:shadow-white/50">
                             Ver proyectos
                         </a>
-                        <a href="/contact"
+                        <a href="https://wa.link/dloqdz"
                             className="px-3 py-2 my-5 transition-all border-2 cursor-pointer text-md w-fit text-secondary border-secondary rounded-xl hover:shadow-xl hover:shadow-secondary" >
                             Contacta conmigo
                         </a>
